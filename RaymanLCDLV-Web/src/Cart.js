ccbbb1c2f68f1e714f3cd35468c091ae1f23ae77
Charlie <luiscarlo.naranjoa@gmail.com>
2024-11-13 11:47:49 -0600

    Aagregar cart para mostrar elementos

--- RaymanLCDLV-Web/src/Cart.js
+++ RaymanLCDLV-Web/src/Cart.js
@@ -1,9 +1,48 @@
-// Cart.js
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 
-const Cart = ({ cartItems, onRemoveFromCart, onPurchase }) => {
-  const navigate = useNavigate();
+const Cart = ({ cartItems, onRemoveFromCart }) => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    // Función para obtener productos desde la API
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch('http://localhost:5000/api/products');
+        if (!response.ok) {
+          throw new Error("Error al obtener productos");
+        }
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error(error.message);
+      }
+    };
+    fetchProducts();
+  }, []);
+
+  const handlePurchase = async () => {
+    const totalAmount = cartItems.reduce((total, item) => total + item.price, 0);
+
+    try {
+      const response = await fetch('http://localhost:5000/api/cart/purchase', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ cartItems, totalAmount }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Error al realizar la compra");
+      }
+
+      const data = await response.json();
+      const orderId = data.orderId;
+      window.location.href = `https://www.sandbox.paypal.com/checkoutnow?token=${orderId}`;
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
 
   return (
     <div className="cart">
@@ -14,17 +53,16 @@ const Cart = ({ cartItems, onRemoveFromCart, onPurchase }) => {
         <ul>
           {cartItems.map((item, index) => (
             <li key={index} className="cart-item">
-              <span>{item.name}</span> - 
-              <span>
-                ${item.price !== undefined ? item.price.toFixed(2) : "0.00"}
-              </span>
+              <img src={item.image} alt={item.name} width="50" />
+              <span>{item.name}</span> - <span>${item.price.toFixed(2)}</span>
+              <p>{item.description}</p>
               <button onClick={() => onRemoveFromCart(index)}>Eliminar</button>
             </li>
           ))}
         </ul>
       )}
       {cartItems.length > 0 && (
-        <button onClick={() => onPurchase(cartItems)}>Comprar</button>
+        <button onClick={handlePurchase}>Comprar</button>
       )}
     </div>
   );
